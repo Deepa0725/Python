c6e034c891b872032757e17289a80e321e4ea0c4
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-25 01:16:36 +0530

    Woked on basics of  variables and datatypes with swapping

--- Basics/2.py
+++ Basics/2.py
@@ -1,1 +1,83 @@
 print("Karnataka")
+print("India")
+
+a = 10
+b = 20
+print(a)
+print(b)
+print(a+b)
+
+p,q,r = 1,2,3
+print(p,q,r)
+
+x=y=z=10
+print(x,y,z)
+
+name = "Deepa"
+Name = "Janya"
+print(name)
+print(Name)
+
+is_switch_on = True
+print(is_switch_on)
+
+city = "Bangalore" #string
+age = 22  #int
+weight = 49.5 #float
+is_student = True #Bool
+
+print(type(city))
+print(type(age))
+print(type(weight))
+print(type(is_student))
+
+is_student = "Yes"
+print(type(is_student))
+
+age1 = float(age)
+print(age1)
+
+l = "100"
+n = "200"
+print(type(l))
+
+# print(l+age)
+# print(l+age1)
+# print(l + n)
+
+print(int(l)+age)
+
+# h = "DEEPA"
+h = 10
+say = 10
+print (int(h) + say)
+
+a = 10
+b = 3
+print(a + b)  # Output: 13
+print(a - b)  # Output: 7
+print(a * b)  # Output: 30
+print(a / b)  # Output: 3.3333...
+print(a // b)  # Output: 3 (Floor Division)
+print(a % b)  # Output: 1 (Modulus/Remainder)
+print(a ** b)  # Output: 1000 (Exponentiation)
+
+print(a+b-a*b/a) #bodmas
+
+a = 10  #With using 3rd Variable
+b = 3
+print("Before Swap:")
+print(a,b)
+temp = a
+a = b
+b = temp
+print("After Swap:")
+print(a,b)
+
+x = 10
+y = 2
+print("Before Swap:")  #without using 3rd variable
+print(x,y)
+x,y=y,x
+print("After Swap:")
+print(x,y)
