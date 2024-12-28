1ac6e3e776556a385ce052d7865eb62ccbb6291a
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-28 20:11:44 +0530

    Added String Concatenation and Repetation

---
+++ Basics/3b.py
@@ -0,0 +1,26 @@
+# '''String Manipulation'''
+# String concatenation
+
+first_name = "DEEPA"
+last_name = "SHREE" 
+full_name = first_name + last_name
+print(full_name)
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+first_word = "Good"
+Second_word = "Morning"
+word = first_word + Second_word
+print(word)
+
+word = first_word + " " + Second_word
+print(word)
+
+
+# Repetation
+message = "Warning! " * 10
+print(message)
+
+NAME1 = "Deepa"
+print(NAME1 * 10)
