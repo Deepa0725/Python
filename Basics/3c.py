aa870fe5a09f89d71e22f09b98d752d8869a4dc8
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-29 00:46:51 +0530

    String Accessing,Slicing ReverseIndex & escape seq

--- Basics/3c.py
+++ Basics/3c.py
@@ -14,3 +14,23 @@ print(abc[0:8])
 print(abc[0:9])
 print(abc[:8])
 print(abc[0:])
+print(abc[2:6])
+
+#Reverse Indexing  [-4,-3,-2,-1]
+print(abc[-4])
+
+#Getting the alernative  [start:end: skip/step]
+print(abc[::])
+print(abc[::1])
+print(abc[::2])
+print(abc[::3])
+print(abc[::4])
+print(abc[::5])
+
+# Escape Sequence (Starts with aback slash)
+s = "Deepa is a \nGood Girl"     #Enter
+print(s)
+
+s = "Deepa is a \tGood Girl"     #Tab
+print(s)
+
