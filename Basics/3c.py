cd7ad2c74d9a0edad4b5155680b02dbcec65f7ba
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-28 20:57:09 +0530

    zxvd

---
+++ Basics/3c.py
@@ -0,0 +1,16 @@
+# Accessing String Characters
+name = "Deepa"
+print(name[1])
+print(name[4])     #Index= position - 1
+print(name[3])
+
+
+# String Slicing
+print(name[2:5])
+
+abc = "Bangalore"
+print(abc[0:7])
+print(abc[0:8])
+print(abc[0:9])
+print(abc[:8])
+print(abc[0:])
