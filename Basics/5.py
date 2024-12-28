a44913404034cf52b170322c8fb0593a35e75eda
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-29 01:59:15 +0530

    HJJKH

---
+++ Basics/5.py
@@ -0,0 +1,32 @@
+# None datatype
+x = None
+print(type(x))
+
+#Operators
+
+# Assignment Operators
+x = 5  # Assigns 5 to x
+print(x)
+
+x += 3  # Equivalent to x = x + 3, now x is 8
+print(x)
+
+x -= 2  # Equivalent to x = x - 2, now x is 6
+print(x)
+
+x *= 4  # Equivalent to x = x * 4, now x is 24
+print(x)
+
+x /= 6  # Equivalent to x = x / 6, now x is 4.0
+print(x)
+
+# Comparison Operators
+a = 10
+b = 20
+
+print(a == b)  # Output: False
+print(a != b)  # Output: True
+print(a > b)  # Output: False
+print(a < b)  # Output: True
+print(a >= 10)  # Output: True
+print(b <= 25)  # Output: True
