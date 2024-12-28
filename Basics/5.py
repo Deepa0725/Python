8de6116b6182fe638749c19491222703f0d074cb
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-29 02:08:54 +0530

    fgh

--- Basics/5.py
+++ Basics/5.py
@@ -30,3 +30,33 @@ print(a > b)  # Output: False
 print(a < b)  # Output: True
 print(a >= 10)  # Output: True
 print(b <= 25)  # Output: True
+
+#  Logical Operators
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not(True))
+print(not(False))
+
+x = 5
+y = 10
+z = 15
+
+# and operator
+print(x > 0 and y > 5)  # Output: True (both conditions are True)
+
+# or operator
+print(x > 10 or z > 10)  # Output: True (one of the conditions is True)
+
+# not operator
+print(not(x > 10))  # Output: True (reverses False to True)
+
+
+# Membership Operators
