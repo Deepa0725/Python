3241df2148f4afb1ccb7e78e6e392016e5f1f177
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-29 02:18:21 +0530

    ghgbjh

--- Basics/5.py
+++ Basics/5.py
@@ -60,3 +60,20 @@ print(not(x > 10))  # Output: True (reverses False to True)
 
 
 # Membership Operators
+my_list = [1, 2, 3, 4, 5]
+my_string = "Python"
+
+print(3 in my_list)  # Output: True (3 is in the list)
+print(6 not in my_list)  # Output: True (6 is not in the list)
+print("P" in my_string)  # Output: True ("P" is in the string)
+print("z" not in my_string)  # Output: True ("z" is not in the string)
+
+string = "Deepa"
+print("D" in string)
+print("D" not in string)
+print("S" not in string)
+
+string1 = "Deepag"
+print("D" in string) and ("D" in string)
+print("D" in string) or ("D" in string)
+print("D" not in string) and ("D"  not in string)
