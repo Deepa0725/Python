28aa98662342e551a7c7bc0f35d2523f3ca33684
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-30 23:19:00 +0530

    hjb

---
+++ Basics/6.py
@@ -0,0 +1,34 @@
+# lists
+
+item1 = "Books"
+item2 = "Pen"                #Inefficient Way
+item3 = "Pencil"
+print(item1, item2, item3)
+
+# Efficient Way
+
+# Syntax
+'''my_list = [element1, element2, element3, ...]'''
+
+# Example1
+items = ["Books", "Pencil", "Pens", "Eraser", "Scale"]
+print(items)
+
+print(items[0])
+print(items[-1])
+
+# Example2
+l = [1,"Books", True, [1,2,3]]
+print(l)
+
+# Example3
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+# Example4
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# Example5
+mixed = ["apple", 3, True]
+print(mixed)
