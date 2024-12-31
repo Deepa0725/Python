269bd62b3e63664811d41d30f4143fa462235215
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-31 22:31:34 +0530

    ghj

--- Basics/6.py
+++ Basics/6.py
@@ -100,3 +100,50 @@ print(numbers[1:4])
 print(numbers[:4])
 print(numbers[2:])  
 print(numbers[::2])
+
+
+l = [100, 200, 300, 400]
+print(l[0:4])
+print(l[0::3])
+
+l2 = l[1:3]           #Subset
+print(l2)
+
+
+# List Functions and Methods
+
+'''Common Functions:'''
+
+# len(list): Returns the number of elements in the list.
+vegetables = ["Tomato", "Potato", "Chilly"]
+print(len(vegetables))
+
+# sorted(list): Returns a new sorted list without changing the original list.
+numbers = [5, 9, 0, 7, 2, 1, 3]
+print(sorted(numbers))
+print(numbers)          # Original list remains unchanged: [5, 2, 9, 1]
+
+# sum(list): Returns the sum of elements in a list (for numerical lists).
+numbers = [1, 2, 3, 4]
+print(sum(numbers))
+print(numbers)
+
+''' Common Methods:'''
+
+# index(element): Returns the index of the first occurrence of the specified element.
+Birds = ["Parrot","Peigon", "Dove", "Peacock", "Crow","Eagle"]
+print(Birds)
+print(Birds.index("Dove"))
+
+# count(element): Returns the number of occurrences of an element in the list.
+numbers = [1, 2, 3, 1, 1]
+print(numbers.count(1))  # Output: 3
+
+# reverse(): Reverses the elements of the list in place.(Descending Order)
+Birds.reverse()
+print(Birds)  #Output:['Eagle', 'Crow', 'Peacock', 'Dove', 'Peigon', 'Parrot']
+
+# sort(): Sorts the list in place (ascending by default).
+numbers = [5, 2, 9, 1]
+numbers.sort()
+print(numbers)  # Output: [1, 2, 5, 9]
