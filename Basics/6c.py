9aec79ecc119309eef1a10ce42fb13fe316b912b
Deepa0725 <deepadevaraj725@gmail.com>
2024-12-31 23:57:11 +0530

    vhv

---
+++ Basics/6c.py
@@ -0,0 +1,19 @@
+# 1. Python list of strings
+groceryList = ["Rice","Wheat","Barley"]
+print(groceryList)
+
+# 2. Python list of numbers
+myNumbers = [1,5,9,16,28]
+print(myNumbers)
+
+# 3. Python list with duplicate values
+myNumbers = [1,5,5,9,9,9,16,28]
+print(myNumbers)
+
+# 4. Python list that contains different data type values
+heterogeneousList = ["Python",123,24.5,True,35,"a"]
+print(heterogeneousList)
+
+# 5. Finding the length of a list in Python
+ProgrammingList = ["CSharp","Python","PHP","R"]
+print(len(ProgrammingList))
