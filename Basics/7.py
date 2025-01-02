e98fc84aaa8effc7dd668c16a6a3b0d489cf645b
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-02 19:14:21 +0530

    dsfdsfsd

--- Basics/7.py
+++ Basics/7.py
@@ -26,10 +26,37 @@ single_element_tuple = ("apple",)
 print(single_element_tuple)
 
 
-# . Accessing Tuple Elements
+#2 . Accessing Tuple Elements
 '''You can access elements in a tuple using indexing, just like with lists. Tuples also support negative indexing.'''
 
 # Example3:
 fruits = ("apple", "banana", "cherry")
 print(fruits[0])  # Output: apple
 print(fruits[-1])  # Output: cherry
+
+# Slicing tuples to acces the subset
+print(fruits[1:3])  # Output: ('banana', 'cherry')            
+
+
+#3 Tuple Operations
+
+# Tuple Concatenation:
+'''You can combine two or more tuples using the + operator.'''
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+combined_tuple = tuple1 + tuple2
+print(combined_tuple)  # Output: (1, 2, 3, 4, 5, 6)
+
+# Tuple Repetition:
+'''You can repeat a tuple multiple times using the * operator.'''
+
+repeated_tuple = (1, 2) * 3
+print(repeated_tuple)  # Output: (1, 2, 1, 2, 1, 2)
+
+# Checking Membership:
+'''You can check if an item exists in a tuple using the ''in'' operator.'''
+
+print("apple" in fruits)  # Output: True
+
+print("hello" in fruits)  #Output: False
