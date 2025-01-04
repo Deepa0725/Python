3ffe40f82a2131df08aec443e493202ee61e5cf8
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-04 16:44:52 +0530

    Dict

--- Basics/8.py
+++ Basics/8.py
@@ -28,6 +28,7 @@ Birthday = {
     "Virat" : "05-1-1988"
 }
 print(Birthday)
+print(type(Birthday))
 
 
 # 2. Accessing Dictionary Elements
@@ -36,7 +37,7 @@ print(Birthday)
 # Example1:(cont)
 print(karnataka_food["Mysuru"])  # Output: Mysore Pak
 
-'''You can also use the get() method to access values, which is safer because it doesn’t throw an error if the key doesn’t exist.'''
+'''can also use the get() method to access values, which is safer because it doesn’t throw an error if the key doesn’t exist.'''
 print(karnataka_food.get("Mangaluru"))  # Output: Neer Dosa
 print(karnataka_food.get("Shivamogga", "Not Found"))  # Output: Not Found
 
@@ -45,3 +46,41 @@ print(Birthday["Janni"])
 
 print(Birthday.get("Deepa"))
 print(Birthday.get("Hello","Not Found"))
+
+
+# 3. Adding and Updating Dictionary Elements
+'''can add new key-value pairs or update existing values in a dictionary.'''
+
+# Adding an Item:
+karnataka_food["Shivamogga"] = "Kadubu"
+print(karnataka_food)
+
+Birthday["Appa"] = "01-11-1969"
+print(Birthday)
+
+# Updating an Item:
+karnataka_food["Bengaluru"] = "Ragi Mudde"
+print(karnataka_food)
+
+
+
+# 4. Removing Elements from a Dictionary
+'''can remove items from a dictionary using several methods:'''
+
+# pop(): Removes the specified key and returns the associated value.
+mysuru_food = karnataka_food.pop("Mysuru")
+print(mysuru_food)  # Output: Mysore Pak
+print(karnataka_food)
+
+print(Birthday)
+x = Birthday.pop("Virat")
+print(Birthday)
+print(x)
+
+# del: Removes the specified key.
+del karnataka_food["Mangaluru"]
+print(karnataka_food)
+
+# clear(): Empties the dictionary.
+karnataka_food.clear()
+print(karnataka_food)
