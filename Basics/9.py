70ddafd1a4a12e8a53f2d20fa9aed56c2bd579ab
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-09 23:47:58 +0530

    sdfsdf

--- Basics/9.py
+++ Basics/9.py
@@ -218,3 +218,17 @@ while pin != correct_pin:
     if pin != correct_pin:
         print("Incorrect PIN. Try again.")
 print("PIN accepted. You can proceed.")
+
+available_seats = 5
+
+while available_seats > 0:
+    print(f"{available_seats} seats available.")
+    booking = input("Do you want to book a seat? (yes/no): ").lower()
+    
+    if booking == "yes":
+        available_seats -= 1
+        print("Seat booked!")
+    else:
+        print("No booking made.")
+
+print("All seats are booked!")
