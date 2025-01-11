0f516c87ba8422c14e494da3134347807b395218
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-11 20:57:28 +0530

    Added elif example

--- Basics/9.py
+++ Basics/9.py
@@ -66,3 +66,44 @@ if (y%2) == 0 :
     print("Number is even") 
 else:
     print("Number is Odd") 
+    
+# Example3:
+signal = 'red'
+
+if signal=="red":
+    print("STOP")
+else:
+    print("GO")
+    
+    # 3. The elif Statement
+'''The elif (short for "else if") statement checks another condition if the previous if or elif condition was False. You can have multiple elif statements to test various conditions.
+'''
+# Syntax:
+'''if condition1:
+    # Code block if condition1 is True
+elif condition2:
+    # Code block if condition2 is True
+else:
+    # Code block if none of the above conditions are True'''
+    
+ # Example1:
+time = 15  # 3 PM
+
+if time == 8:
+    print("It's breakfast time!")            #If the time is 8 AM, it prints "It's breakfast time!"
+elif time == 13:
+    print("It's lunch time!")                #If the time is 1 PM, it prints "It's lunch time!".
+elif time == 20:
+    print("It's dinner time!")               #If the time is 8 PM, it prints "It's dinner time!".
+else:
+    print("It's not a meal time.")           #If none of these conditions are true, it prints "It's not a meal time."
+    
+    # Example2:
+signal = "green"
+
+if signal == "red":
+    print("STOP")
+elif signal == "yellow":
+    print("READY")
+else:
+    print("GO")
