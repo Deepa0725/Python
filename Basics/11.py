b4ab0c5d14a564ad156382b24fb8b245d4285cad
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-13 23:59:07 +0530

    hjbhb

--- Basics/11.py
+++ Basics/11.py
@@ -36,3 +36,16 @@ when you want to repeat a block of code a specific number of times.
 # Example1: Counting from 1 to 10
 for i in range(1, 11):
     print(i)
+
+# Output:
+'''
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10'''
