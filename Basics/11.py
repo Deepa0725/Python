706502779e9fdacb1719229b0faac48e563a8bdd
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-18 23:39:10 +0530

    gdfgd

---
+++ Basics/11.py
@@ -0,0 +1,10 @@
+# While Loops in Python
+'''A loop is a programming structure that repeats a set of instructions as long as a specified condition is True. 
+In Python, the while loop allows you to repeatedly execute a block of code as long as the condition is True.'''
+
+
+# 1. The Basic Structure of a while Loop
+'''The while loop repeatedly executes a block of code as long as the condition is True.
+'''
+# Syntax:
+'''while condition:    '''           # Code to execute as long as condition is True
