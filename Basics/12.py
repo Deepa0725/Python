d75a0e04e93f8529231d68da298f41de41dec41e
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-18 23:42:38 +0530

    gfdgd

---
+++ Basics/12.py
@@ -0,0 +1,11 @@
+# EXAMPLES
+# 1 Basic Conditions:
+
+'''# Write a program to check if someone is eligible for a bus pass. 
+If they are below 5 years, the bus pass is free. 
+If they are 60 years or older, they get a senior citizen discount.
+Otherwise, they pay the full price.
+'''
+# program
+person = input("Enter the person: ")
+age = int(input("Enter the age: "))
