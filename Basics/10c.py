75f2f7481361515c06165b949b5cdcf1eb7a92d2
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-28 23:14:37 +0530

    loop

---
+++ Basics/10c.py
@@ -0,0 +1,26 @@
+# Practice Programs
+
+# 1.Basic Counting with while Loop:
+'''Write a program that counts from 1 to 10 using a while loop.'''
+
+i = 0
+
+while i <= 10:
+    print(i)
+    i += 1
+    
+# OUTPUT:
+'''
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'''
+
