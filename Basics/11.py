1f2009a080405555c5ec9ac79582f8fb0d5c8854
Deepa0725 <deepadevaraj725@gmail.com>
2025-01-29 21:56:23 +0530

    gvcg

--- Basics/11.py
+++ Basics/11.py
@@ -63,4 +63,40 @@ for i in range(1, 11):
 9
 10'''
 
+# Example2: Same above program using while loop
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# output:
+'''
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10'''
+
+
+# Example3: Counting by 2s from 1 to 10
+
+for i in range(1, 11, 2):
+    print(i)
+    
+'''This loop prints only the odd numbers between 1 and 10.
+'''
+# Output:
+'''
+1
+3
+5
+7
+9'''
+
+
 
